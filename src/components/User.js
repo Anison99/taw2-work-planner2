05fc166545e96c4c7aabf8fbc2df20a96758b37a
anison99_ <anita.zych9941@gmail.com>
2023-12-05 14:48:28 +0100

    add editing and deleting tasks

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,190 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Navbar from './Navbar';
+import '../css/User.css';
 
 function User() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [projects, setProjects] = useState([]);
+  const [editedProject, setEditedProject] = useState({
+    projectId: '',
+    nameOfProject: '',
+    description: '',
+  });
+  const [isEditing, setIsEditing] = useState(false); // Stan określający, czy jesteśmy w trybie edycji
+
+  const fetchUserProjects = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/user/projects', { withCredentials: true });
+      setProjects(response.data.projects);
+    } catch (error) {
+      console.error('Error fetching user projects:', error);
+    }
+  };
+
+  useEffect(() => {
+    // Pobranie nazwy użytkownika
+    const fetchUsername = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/user', { withCredentials: true });
+        setUsername(response.data.username);
+      } catch (error) {
+        console.error('Error fetching username:', error);
+      }
+    };
+
+    // Pobranie adresu email użytkownika
+    const fetchUserEmail = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/user/email', { withCredentials: true });
+        setEmail(response.data.email);
+      } catch (error) {
+        console.error('Error fetching email:', error);
+      }
+    };
+
+    fetchUsername();
+    fetchUserEmail();
+    fetchUserProjects();
+  }, []);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditedProject({ ...editedProject, [name]: value });
+  };
+
+  const handleEditProject = async (projectId) => {
+    try {
+      const response = await axios.put(`http://localhost:5000/api/projects/${projectId}`, editedProject, {
+        withCredentials: true,
+      });
+
+      if (response.status === 200) {
+        console.log('Project updated successfully');
+        fetchUserProjects();
+        setEditedProject({
+          projectId: '',
+          nameOfProject: '',
+          description: '',
+        });
+        setIsEditing(false); // Po zapisaniu zmian, wyłączamy tryb edycji
+      } else {
+        console.error('Error updating project');
+      }
+    } catch (error) {
+      console.error('Error updating project:', error);
+    }
+  };
+
+  const handleDeleteProject = async (projectId) => {
+    try {
+      const response = await axios.delete(`http://localhost:5000/api/projects/${projectId}`, {
+        withCredentials: true,
+      });
+
+      if (response.status === 200) {
+        console.log('Project deleted successfully');
+        fetchUserProjects();
+      } else {
+        console.error('Error deleting project');
+      }
+    } catch (error) {
+      console.error('Error deleting project:', error);
+    }
+  };
+
+
   return (
     <div>
-      
+      <Navbar />
+      <div className="user-container">
+        <h2 className='user-header'>Profil użytkownika {username}</h2>
+        <h2>Twoje projekty</h2>
+        <ul className="project-list">
+          {projects.map((project) => (
+            <li className="project-item" key={project._id}>
+              <h3>Nazwa: {project.nameOfProject}</h3>
+              <p>Opis: {project.description}</p>
+              <button
+                className="btn btn-outline-primary"
+                onClick={() => {
+                  setEditedProject({ ...editedProject, projectId: project._id });
+                  setIsEditing(true);
+                }}>
+                Edytuj
+              </button>
+              <button
+                className="btn btn-outline-danger" // Przycisk do usuwania projektu
+                onClick={() => handleDeleteProject(project._id)}>
+                Usuń
+              </button>
+            </li>
+          ))}
+
+        </ul>
+        {isEditing && (
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleEditProject(editedProject.projectId);
+            }}
+            style={{ width: '100%' }}
+          >
+            <h2>Edytuj projekt</h2>
+            <div style={{ width: '100%', marginTop: '20px' }}>
+              <input
+                type="text"
+                name="nameOfProject"
+                placeholder="Nowa nazwa"
+                value={editedProject.nameOfProject}
+                onChange={handleInputChange}
+                style={{
+                  width: '100%',
+                  padding: '10px',
+                  border: '1px solid #ccc',
+                  borderRadius: '4px',
+                }}
+              />
+            </div>
+            <div style={{ width: '100%', marginTop: '20px' }}>
+              <textarea
+                name="description"
+                placeholder="Nowy opis"
+                value={editedProject.description}
+                onChange={handleInputChange}
+                style={{
+                  width: '100%',
+                  padding: '10px',
+                  border: '1px solid #ccc',
+                  borderRadius: '4px',
+                }}
+              ></textarea>
+            </div>
+            <div style={{ marginTop: '20px' }}>
+              <button
+                type="button"
+                className="btn btn-outline-secondary"
+                onClick={() => {
+                  setIsEditing(false);
+                  setEditedProject({
+                    projectId: '',
+                    nameOfProject: '',
+                    description: '',
+                  });
+                }}
+              >
+                Anuluj
+              </button>
+              <button type="submit" className="btn btn-outline-primary">
+                Zapisz zmiany
+              </button>
+            </div>
+          </form>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default User
+export default User;
--- src/server.js
+++ src/server.js
@@ -13,95 +13,95 @@ const sessionSecret = process.env.SESSION_SECRET || 'default-secret-key';
 
 
 app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
+  origin: 'http://localhost:3000',
+  credentials: true,
 }));
 app.use(express.json());
 app.use(session({
-    secret: sessionSecret,
-    resave: false,
-    saveUninitialized: false,
+  secret: sessionSecret,
+  resave: false,
+  saveUninitialized: false,
 }));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
 mongoose.connect('mongodb://localhost:27017/planner', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Błąd połączenia z MongoDB:'));
 db.once('open', () => {
-    console.log('Połączenie z MongoDB zostało nawiązane');
+  console.log('Połączenie z MongoDB zostało nawiązane');
 });
 
 // ------------ Definicja schematu użytkownika
 const userSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
-    crearedAt: Date,
+  username: String,
+  email: String,
+  password: String,
+  crearedAt: Date,
 });
 
 const User = mongoose.model('User', userSchema);
 
 // ------------ Definicja schematu dla logowania użytkownika 
 const workLogsSchema = new mongoose.Schema({
-    userId: String,
-    projectId: String,
-    projectName: String,
-    startTime: String,
-    endTime: String,
-    startBreakTime: String,
-    endBreakTime: String,
+  userId: String,
+  projectId: String,
+  projectName: String,
+  startTime: String,
+  endTime: String,
+  startBreakTime: String,
+  endBreakTime: String,
 });
 
 const WorkLog = mongoose.model('WorkLogs', workLogsSchema);
 
 // ------------ Definicja schematu dla projektów
 const projectsSchema = new mongoose.Schema({
-    userId: String,
-    nameOfProject: String,
-    description: String,
+  userId: String,
+  nameOfProject: String,
+  description: String,
 });
 
 const Project = mongoose.model('Projects', projectsSchema);
 
 passport.use(new LocalStrategy(
-    { usernameField: 'email' },
-    async (email, password, done) => {
-        try {
-            const user = await User.findOne({ email });
+  { usernameField: 'email' },
+  async (email, password, done) => {
+    try {
+      const user = await User.findOne({ email });
 
-            if (!user) {
-                return done(null, false, { message: 'Invalid email' });
-            }
+      if (!user) {
+        return done(null, false, { message: 'Invalid email' });
+      }
 
-            const isPasswordValid = await bcrypt.compare(password, user.password);
+      const isPasswordValid = await bcrypt.compare(password, user.password);
 
-            if (!isPasswordValid) {
-                return done(null, false, { message: 'Invalid password' });
-            }
+      if (!isPasswordValid) {
+        return done(null, false, { message: 'Invalid password' });
+      }
 
-            return done(null, user);
-        } catch (err) {
-            return done(err);
-        }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
     }
+  }
 ));
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser(async (id, done) => {
-    try {
-        const user = await User.findById(id);
-        done(null, user);
-    } catch (err) {
-        done(err);
-    }
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
 });
 
 const bodyParser = require('body-parser'); // Dodanie bodyParser do parsowania danych z żądania
@@ -144,29 +144,29 @@ app.post('/register', async (req, res) => {
 
 // Kod obsługi logowania użytkownika
 app.post('/login', async (req, res, next) => {
-    passport.authenticate('local', async (err, user, info) => {
-      try {
-        if (err) {
-          return res.status(500).json({ message: 'Internal Server Error' });
-        }
-        if (!user) {
-          return res.status(401).json({ message: 'Invalid credentials' });
-        }
-        req.logIn(user, (err) => {
-          if (err) {
-            return res.status(500).json({ message: 'Login failed' });
-          }
-          console.log('User logged in:', req.user.username);
-          return res.json({ message: 'Login successful' });
-        });
-      } catch (error) {
-        console.error('Login error:', error);
-        res.status(500).json({ message: 'Error during login' });
+  passport.authenticate('local', async (err, user, info) => {
+    try {
+      if (err) {
+        return res.status(500).json({ message: 'Internal Server Error' });
       }
-    })(req, res, next);
-  });
+      if (!user) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+      }
+      req.logIn(user, (err) => {
+        if (err) {
+          return res.status(500).json({ message: 'Login failed' });
+        }
+        console.log('User logged in:', req.user.username);
+        return res.json({ message: 'Login successful' });
+      });
+    } catch (error) {
+      console.error('Login error:', error);
+      res.status(500).json({ message: 'Error during login' });
+    }
+  })(req, res, next);
+});
 
-  // Rejestracja projektu
+// Rejestracja projektu
 app.post('/api/projects', async (req, res) => {
   try {
     const { nameOfProject, description } = req.body;
@@ -217,7 +217,7 @@ app.get('/api/user', async (req, res) => {
     }
 
     const user = await User.findById(req.user.id);
-    
+
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
@@ -227,6 +227,61 @@ app.get('/api/user', async (req, res) => {
   } catch (error) {
     console.error('Error fetching user:', error);
     res.status(500).json({ message: 'Error fetching user' });
+  }
+});
+
+// Endpoint do pobierania adresu email zalogowanego użytkownika
+app.get('/api/user/email', async (req, res) => {
+  try {
+    if (!req.isAuthenticated()) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Zwracanie adresu email zalogowanego użytkownika
+    res.status(200).json({ email: user.email });
+  } catch (error) {
+    console.error('Error fetching user email:', error);
+    res.status(500).json({ message: 'Error fetching user email' });
+  }
+});
+
+// Aktualizacja danych projektu
+app.put('/api/projects/:projectId', async (req, res) => {
+  try {
+    const { nameOfProject, description } = req.body;
+    const projectId = req.params.projectId;
+
+    const updatedProject = await Project.findByIdAndUpdate(projectId, { nameOfProject, description }, { new: true });
+
+    if (!updatedProject) {
+      return res.status(404).json({ message: 'Project not found' });
+    }
+
+    res.status(200).json({ message: 'Project updated successfully', updatedProject });
+  } catch (error) {
+    console.error('Error updating project:', error);
+    res.status(500).json({ message: 'Error updating project' });
+  }
+});
+
+//usuwanie wybranego projektu
+app.delete('/api/projects/:id', async (req, res) => {
+  try {
+    const projectId = req.params.id;
+
+    // Usunięcie projektu o podanym ID
+    await Project.findByIdAndRemove(projectId);
+
+    res.status(200).json({ message: 'Project deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting project:', error);
+    res.status(500).json({ message: 'Error deleting project' });
   }
 });
 
