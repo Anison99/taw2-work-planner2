1cfbe72f9c6de492c657201723b805abf33804a1
anison99_ <anita.zych9941@gmail.com>
2023-12-06 10:46:10 +0100

    add breaks rejestration

--- src/components/TimeRegister.js
+++ src/components/TimeRegister.js
@@ -3,16 +3,20 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import Navbar from './Navbar';
 
+import '../css/TimeRegister.css';
+
 function TimeRegister() {
   const [startTime, setStartTime] = useState('');
   const [endTime, setEndTime] = useState('');
+  const [breakStartTime, setBreakStartTime] = useState('');
+  const [breakEndTime, setBreakEndTime] = useState('');
   const [projectName, setProjectName] = useState('');
   const [projects, setProjects] = useState([]);
   const [isWorking, setIsWorking] = useState(false);
+  const [isOnBreak, setIsOnBreak] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Pobranie projektów z bazy danych przy załadowaniu komponentu
     const fetchProjects = async () => {
       try {
         const response = await axios.get('http://localhost:5000/api/user/projects', { withCredentials: true });
@@ -35,8 +39,27 @@ function TimeRegister() {
 
   const handleStop = () => {
     const currentDate = new Date().toLocaleString();
-    setEndTime(currentDate);
     setIsWorking(false);
+  
+    if (!isOnBreak) {
+      setEndTime(currentDate);
+    } else {
+      setBreakEndTime(currentDate);
+    }
+  };
+  
+
+
+  const handleStartBreak = () => {
+    const currentDate = new Date().toLocaleString();
+    setBreakStartTime(currentDate);
+    setIsOnBreak(true);
+  };
+
+  const handleStopBreak = () => {
+    const currentDate = new Date().toLocaleString();
+    setBreakEndTime(currentDate);
+    setIsOnBreak(false);
   };
 
   const handleSubmit = async (e) => {
@@ -48,6 +71,8 @@ function TimeRegister() {
           startTime,
           endTime,
           projectName,
+          startBreakTime: breakStartTime,
+          endBreakTime: breakEndTime,
         },
         { withCredentials: true }
       );
@@ -61,34 +86,47 @@ function TimeRegister() {
       console.error('Error registering time:', error);
     }
   };
+  
 
   return (
     <div>
       <Navbar />
-      <h2>Time Register</h2>
-      <p>Start Time: {startTime}</p>
-      <p>End Time: {endTime}</p>
-      {!isWorking ? (
-        <button onClick={handleStart}>Start</button>
-      ) : (
-        <button onClick={handleStop}>Stop</button>
-      )}
-      <form onSubmit={handleSubmit}>
-        <label>
-          Project Name:
-          <select value={projectName} onChange={(e) => setProjectName(e.target.value)}>
-            <option value="">Select a project</option>
-            {projects.map((project) => (
-              <option key={project._id} value={project.nameOfProject}>
-                {project.nameOfProject}
-              </option>
-            ))}
-          </select>
-        </label>
-        <button type="submit" disabled={!endTime || !projectName}>
-          Register Time
-        </button>
-      </form>
+      <div className="time-container">
+        <h2 className="time-header">Time Register</h2>
+        <p>Start Time: {startTime}</p>
+        <p>End Time: {endTime}</p>
+        <h2 className="time-header">Breaks</h2>
+        <p>Start Time: {breakStartTime}</p>
+        <p>End Time: {breakEndTime}</p>
+        {!isWorking ? (
+          <button className="btn btn-outline-success" onClick={handleStart}>Start</button>
+        ) : (
+          <>
+            <button className="btn btn-outline-danger" onClick={handleStop}>Stop</button>
+            {!isOnBreak ? (
+              <button className="btn btn-outline-warning" onClick={handleStartBreak}>Start Break</button>
+            ) : (
+              <button className="btn btn-outline-secondary" onClick={handleStopBreak}>End Break</button>
+            )}
+          </>
+        )}
+        <form className="time-form" onSubmit={handleSubmit}>
+          <label>
+            Project Name:
+            <select className="time-select" value={projectName} onChange={(e) => setProjectName(e.target.value)}>
+              <option value="">Select a project</option>
+              {projects.map((project) => (
+                <option key={project._id} value={project.nameOfProject}>
+                  {project.nameOfProject}
+                </option>
+              ))}
+            </select>
+          </label>
+          <button className="time-submit btn btn-outline-primary" type="submit" disabled={!endTime || !projectName || isWorking}>
+            Register Time
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
